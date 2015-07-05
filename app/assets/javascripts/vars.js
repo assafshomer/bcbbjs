4828eb5658e8eefc1d2dc7d50c63ec5162df0c17
Assaf Shomer <assafshomer@gmail.com>
2015-07-05 14:51:29 +0300

    getpixel

--- app/assets/javascripts/vars.js
+++ app/assets/javascripts/vars.js
@@ -2,9 +2,11 @@ var BOARD_WIDTH = 1250; // LAYOUT SIZE
 var BOARD_HEIGHT = 800;
 var GRID_SIZE = 1; // AIDING TO PLACE IMAGES WITH BETTER ALIGNMENT, THOUGH ISN'T NECESSARY AND CAN BE SET TO 1
 var MOUSE_STALE_TIME = 400; // REQUEST URLS FROM SERVER ONLY IF MOUSE IS STALE FOR N MILLISECONDS
-var SERVER_ROOT = "http://www.blockchainbillboard.com";
+// var SERVER_ROOT = "http://www.blockchainbillboard.com";
+var SERVER_ROOT = "http://localhost:3000";
 var SERVER_BOARDIMAGE_URL = "https://blockchainbillboard-contabo.s3.amazonaws.com/bcbb_image.png?v=" + Math.random();
-var SERVER_GETPIXEL_URL = "getpixel.html"; // FOR DEMONSTRATION ONLY. REPLACE WITH A DYNAMIC HANDLER
+// var SERVER_GETPIXEL_URL = "getpixel.html";
+var SERVER_GETPIXEL_URL = "http://localhost:3001/getpixel";
 var SERVER_GETPRICE_URL = SERVER_ROOT+"/api/pixels";
 
 var REQUEST_TIMEOUT = 999999; // ms
