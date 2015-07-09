e5ea3b7bd79a749e7be49accc09c5119871db318
Assaf Shomer <assafshomer@gmail.com>
2015-07-09 15:44:38 +0300

    Added app name to info to avoid confusion

--- app/controllers/api/info_controller.rb
+++ app/controllers/api/info_controller.rb
@@ -1,6 +1,6 @@
 class Api::InfoController < Api::BaseController
 
   def index  	
-		render json: { version: "#{APP_CONFIG['version']}", launch_time: "#{Time.now.strftime("%Y/%m/%d %H:%M:%S")}", dir: "#{Rails.root.to_s.split('/').last}", aws_bucket: "#{APP_CONFIG['aws_bucket']}",backend_ip:"http://"+"#{APP_CONFIG['bcbb_server']}"+":"+"#{APP_CONFIG['bcbb_port']}"}.to_json, status: 200
+		render json: { application: "#{Rails.application.class.parent_name}",version: "#{APP_CONFIG['version']}", launch_time: "#{Time.now.strftime("%Y/%m/%d %H:%M:%S")}", dir: "#{Rails.root.to_s.split('/').last}", aws_bucket: "#{APP_CONFIG['aws_bucket']}",backend_ip:"http://"+"#{APP_CONFIG['bcbb_server']}"+":"+"#{APP_CONFIG['bcbb_port']}"}.to_json, status: 200
   end
 end
