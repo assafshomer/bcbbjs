3ed38a8d50dba8ed24d7289c0ceebb29b25cc56b
Assaf Shomer <assafshomer@gmail.com>
2015-07-14 10:49:06 +0300

    titles to server links

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -531,8 +531,8 @@ $(document).ready(function () {
     initBoard();
     loadBoardImage();
     initGUI();
-    $("#serverroot").val(SERVER_ROOT);
-    $("#toshroot").val(TOSHI_SERVER);
+    $("#serverroot").val("Backend Server: "+SERVER_ROOT);
+    $("#toshroot").val("Toshi Server: "+TOSHI_SERVER);
     $("#serverroot").change(function () {
         SERVER_ROOT = $(this).val();
         SERVER_GETPRICE_URL = SERVER_ROOT + "/api/pixels";
